c258a605e93254809661e56570b34efba24d2606
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-10-27 19:31:16 -0400

    fix: rearrange

--- sortWordsByLastCharacter.js
+++ sortWordsByLastCharacter.js
@@ -1,3 +1,52 @@
+function sortWordsByLastCharacter(stringOfMultipleWords){ 
+    let wordsArray = stringOfMultipleWords.split(' ')
+    wordsArray.sort((previousWord, currentWord) => { 
+        return getLastCharCode(previousWord) - getLastCharCode(currentWord)
+    })
+    return wordsArray
+}
+function getLastCharCode(word){ 
+    return word.slice(-1).charCodeAt(0)
+}
+
+
+function test(){
+console.log(sortWordsByLastCharacter('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+console.log(sortWordsByLastCharacter('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
+console.log(sortWordsByLastCharacter('take me to semynak'), ['take', 'me', 'semynak', 'to'] );
+}
+
+
+test();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
 
@@ -9,17 +58,3 @@ last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 
 last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
 last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
 */
-
-function sortWordsByLastCharacter(stringOfMultipleWords){ 
-    let wordsArray = stringOfMultipleWords.split(' ')
-    wordsArray.sort((previousWord, currentWord) => { 
-        return getLastCharCode(previousWord) - getLastCharCode(currentWord)
-    })
-    return wordsArray
-}
-function getLastCharCode(word){ 
-    return word.slice(-1).charCodeAt(0)
-}
-console.log(sortWordsByLastCharacter('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
-console.log(sortWordsByLastCharacter('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
-console.log(sortWordsByLastCharacter('take me to semynak'), ['take', 'me', 'semynak', 'to'] );
