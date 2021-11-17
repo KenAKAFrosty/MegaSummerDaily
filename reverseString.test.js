bce50b9348f0665f35fa3d9f009c2e8769c7259a
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-16 20:48:32 -0500

    add: reverseString()  but solely as test file this time

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,4 +1,26 @@
-import {reverseString} from "./reverseString.js"
-describe("a string argument returns a string", () => { 
-    expect(typeof reverseString("hello")).toBe("string");
+function reverseString(str) {
+    if (typeof str !== "string")
+        return null;
+    let reversedChars = "";
+    for (let i = str.length-1; i >= 0; i--){  
+        reversedChars += str[i];
+    }
+    return reversedChars;
+}
+
+describe("reverseString", () => {
+  test("returns null when given an integer argument", () => {
+    expect(reverseString(42)).toBe(null);
+  });
+  test("returns null when given an array argument", () => {
+    expect(reverseString(["array","of","strings"])).toBe(null);
+  });
+  test("reverses 'cat mom'", () => {
+    expect(reverseString('cat mom')).toBe('mom tac');
+  });
+  test("reverses '' (sends back empty string)", () => {
+    expect(reverseString('')).toBe('');
+  });
 });
+
+//Given a string of characters as input, write a function that returns it with the characters reversed. No Reverse Method (well brute force it first, but then no reverse method)!
